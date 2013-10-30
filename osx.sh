35d990f40f91e4adfd267538e6a63585938ed52b
Neil Gupta <neil@tabuleapp.com>
2013-10-29 20:02:30 -0500

    Clean up osx.sh

--- osx.sh
+++ osx.sh
@@ -1,24 +1,8 @@
-# Disable Expose animations.
-# defaults write com.apple.dock expose-animation-duration -int 0
-
-# Disable Launchpad animations.
-# defaults write com.apple.dock springboard-show-duration -int 0
-# defaults write com.apple.dock springboard-hide-duration -int 0
-
-# Disable auto-hiding dock delay.
-# defaults write com.apple.Dock autohide-delay -float 0
-
 # Enable Text Selection within QuickLook
 defaults write com.apple.finder QLEnableTextSelection -boolean YES
 
-# Enable hidden four-finger double-tap gesture to go back to most recent space.
-defaults write com.apple.dock double-tap-jump-back -bool TRUE
-
 # Make Dock icons of hidden applications translucent.
 defaults write com.apple.dock showhidden -bool true
-
-# Add a context menu item for showing the Web Inspector in web views.
-#defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 # Copy just the email address in Mail, not name <name@email.com> format
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
