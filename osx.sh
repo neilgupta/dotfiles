a21b8e48db2f58e067e5143230c716b105d58c8e
Neil Gupta <neil@tabuleapp.com>
2013-11-08 08:48:20 -0600

    Remove AddressesIncludeNameOnPasteboard terminal command because it no longer seems to work in Mavericks

--- osx.sh
+++ osx.sh
@@ -4,8 +4,4 @@ defaults write com.apple.finder QLEnableTextSelection -boolean YES
 # Make Dock icons of hidden applications translucent.
 defaults write com.apple.dock showhidden -bool true
 
-# Copy just the email address in Mail, not name <name@email.com> format
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
 killall Dock
-echo "...done. Some changes may require a restart to take effect."
