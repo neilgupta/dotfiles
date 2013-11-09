22422df4abeba1d5201b4d4d8095bc97b249bc8c
Neil Gupta <neil@tabuleapp.com>
2013-11-09 02:35:58 -0600

    Add heroku-toolbelt and noejectdelay to my apps install list

--- apps.sh
+++ apps.sh
@@ -6,12 +6,14 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Apps
-brew cask install dropbox
-brew cask install google-chrome
+brew cask install heroku-toolbelt
 brew cask install keyremap4macbook
 brew cask install pc-keyboardhack
+brew cask install noejectdelay
 brew cask install slate
 brew cask install splashtop-streamer
+brew cask install dropbox
+brew cask install google-chrome
 brew cask install sequel-pro
 brew cask install intellij-community
 brew cask install hipchat
@@ -23,3 +25,6 @@ ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
 
 # Postgres
 # brew install postgres
+
+# RVM
+# Install rvm, ruby 1.9.3, bundler
