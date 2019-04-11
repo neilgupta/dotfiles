17cfa39125b8bd6e0fb205eb63031203f9e70134
Neil Gupta <neil@hiome.com>
2019-04-11 16:34:29 -0500

    change default git email, drop sublime theming

--- osx.sh
+++ osx.sh
@@ -4,7 +4,4 @@ defaults write com.apple.finder QLEnableTextSelection -boolean YES
 # Make Dock icons of hidden applications translucent.
 defaults write com.apple.dock showhidden -bool true
 
-# Disable guest account
-sudo defaults write /Library/Preferences/com.apple.loginwindow.plist "GuestEnabled" 0
-
 # killall Dock
